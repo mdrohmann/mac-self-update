e8a16e60816508a4c521e9c0d52e9cbbb20b676d
Martin C Drohmann <martind@activestate.com>
2021-08-27 12:50:57 -0700

    does rename work too?

--- cmd/installer/main.go
+++ cmd/installer/main.go
@@ -1,41 +1,20 @@
 package main
 
 import (
-	"io"
+	"fmt"
 	"log"
 	"os"
 )
 
 func main() {
-	if err := os.Remove("./start"); err != nil {
-		log.Fatalf("Failed to remove start: %v", err)
-	}
-	if err := copy("./stop", "start"); err != nil {
+	if err := copyRename("./stop", "start"); err != nil {
 		log.Fatalf("Failed to copy stop to start: %v", err)
 	}
 }
 
-func copy(src, dst string) error {
-	in, err := os.Open(src)
-	if err != nil {
-		return err
+func copyRename(src, dst string) error {
+	if err := os.Chmod(src, 0755); err != nil {
+		return fmt.Errorf("failed to make file executable: %v", err)
 	}
-	defer in.Close()
-
-	out, err := os.Create(dst)
-	if err != nil {
-		return err
-	}
-
-	_, err = io.Copy(out, in)
-	if err != nil {
-		out.Close()
-		return err
-	}
-
-	if err := out.Close(); err != nil {
-		return err
-	}
-
-	return os.Chmod(dst, 0755)
+	return os.Rename(src, dst)
 }
--- test.sh
+++ test.sh
@@ -6,8 +6,12 @@ go build -o ./installer ./cmd/installer
 
 ./start installer
 
+echo
 echo "Test without installer"
+echo
+
 
 go build -o ./start ./cmd/start
+go build -o ./stop ./cmd/stop
 
 ./start
