db4e3c9b063955d25ab79c9cee7755dc0c61d7ac
Martin C Drohmann <martind@activestate.com>
2021-08-27 14:41:20 -0700

    copying again

--- cmd/installer/main.go
+++ cmd/installer/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
@@ -21,8 +22,37 @@ func main() {
 }
 
 func copyRename(src, dst string) error {
-	if err := os.Chmod(src, 0755); err != nil {
+	if err := copyFile(src, dst); err != nil {
+		return fmt.Errorf("failed to copy file to destination: %v", err)
+	}
+	if err := os.Chmod(dst, 0755); err != nil {
 		return fmt.Errorf("failed to make file executable: %v", err)
 	}
 	return os.Rename(src, dst)
+}
+
+func copyFile(src, target string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return fmt.Errorf("os.Open %s failed: %v", src, err)
+	}
+	defer in.Close()
+
+	// Create target file
+	out, err := os.Create(target)
+	if err != nil {
+		return fmt.Errorf("os.Create %s failed: %v", target, err)
+	}
+	defer out.Close()
+
+	// Copy bytes to target file
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return fmt.Errorf("io.Copy failed: %v", err)
+	}
+	err = out.Close()
+	if err != nil {
+		return fmt.Errorf("out.Close failed: %v", err)
+	}
+	return nil
 }
